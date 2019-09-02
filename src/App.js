a40e63981d8d1e7a9dff2a38d1d9507338713449
bryantb2 <bryantb2@my.lanecc.edu>
2019-09-02 15:36:10 -0700

    added info divider; finished highlight photo grid

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 render() {
   return (
       <div>
-        <div id="page1">
+        <section id="page1">
         
         <img className="titleImage d-block img-fluid mx-auto" src={textBoxVector}/>
 
@@ -61,67 +61,70 @@ render() {
         <div className="d-flex scroll-info justify-content-center">
             <div className="p-2 scroll-text">Scroll Down for Mainpage</div>
         </div>
-      </div>
+      </section>
       
 
       <div className="page-divider">
         <nav className="navbar navbar-expand-lg navbar-dark  customNavStyles-unFixed">
-      <div className="container-fluid">
-          <a className="navbar-brand" href="#">Bryant Photography</a>
+      
+            <div className="container-fluid">
+      
+              <a className="navbar-brand" href="#">Bryant Photography</a>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto centeringStyle">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Highlights<span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Portfolio<span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Share<span className="sr-only">(current)</span></a>
-                </li>
-            </ul>
-          </div>
-        </div>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto centeringStyle">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Highlights<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Portfolio<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Share<span className="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+              </div>
+    
+            </div>
+    
         </nav>
       </div>
     
 
-      <div id="page2">
-      <div id="imageContainer">
-        <img className="mainImage" src={backgroundImage}/>
-      </div>
-        <article className="contentContainer">
-            <div className="d-flex justify-content-center headerStyle align-self-center">
-                <div className="p-2">Bryant Photography</div>
-            </div>
+      <section id="page2">
+          <div id="imageContainer">
+            <img className="mainImage" src={backgroundImage}/>
+          </div>
+            <article className="contentContainer">
+                <div className="d-flex justify-content-center headerStyle align-self-center">
+                    <div className="p-2">Bryant Photography</div>
+                </div>
 
-            <div className="d-flex justify-content-center infoStyle">
-                  <div className="p-2">Emphasis on drone shots and natural environments.</div>
-            </div>
+                <div className="d-flex justify-content-center infoStyle">
+                      <div className="p-2">Emphasis on drone shots and natural environments.</div>
+                </div>
 
-            <div className="d-flex justify-content-center lineStyle">
-                  <div className="p-2">&nbsp;</div>
-            </div>
+                <div className="d-flex justify-content-center lineStyle">
+                      <div className="p-2">&nbsp;</div>
+                </div>
 
-            <div className="d-flex justify-content-center">
-                  <button type="button" className="text-center btn btn-lg btn-light ">Portfolio</button>
-            </div>
-        </article>
-
-        </div>
+                <div className="d-flex justify-content-center">
+                      <button type="button" className="text-center btn btn-lg btn-light ">Portfolio</button>
+                </div>
+            </article>
+        </section>
         
-        <div id="highlightsDivider1">
+        <div id="highlightsDivider">
             <div className="d-flex justify-content-center align-self-center dividerHeader">
                 <div className="p-2">Portfolio Highlights</div>
             </div>
@@ -129,63 +132,78 @@ render() {
             <div className="d-flex justify-content-center align-self-center dividerSubText">
                 <div className="p-2">Click photos for expanded view.</div>
             </div>
-            
         </div>
         
-        <div id="page3">
+    
+        <section id="page3">
             <div className="row">
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
 
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
             </div>
              <div className="row">
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
 
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
             </div>
             <div className="row">
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
 
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
-                <div className="col-lg-4 col-md-4 col-6">
+                <div className="col-lg-4 col-md-4 col-4">
                     <a href="#" className="d-block mb-4 h-100">
                         <img className="imageAnchor img-thumbnail" src={highlight1} alt=""/>
                     </a>
                 </div>
+            </div>
+        </section>
+        
+        <div id="moreInfoDivider">
+            <div className="d-flex justify-content-center align-self-center dividerSubText">
+                <div className="p-2">For more of my work, visit the portfolio section of this site</div>
+            </div>
+    
+            <div className="d-flex justify-content-center infoDivider-lineStyle">
+                <div className="p-2">&nbsp;</div>
+            </div>
+            
+    
+            <div className="d-flex justify-content-center">
+                  <button type="button" className="text-center btn btn-lg btn-light ">Portfolio</button>
             </div>
         </div>
         
