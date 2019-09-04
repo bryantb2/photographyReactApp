0d285ec99066ff836fcee12fbbc973a6dc359e30
bryantb2 <bryantb2@my.lanecc.edu>
2019-09-03 18:17:04 -0700

    added contact page JSX, structure will be improved

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import backgroundImage from '../src/referenceImages/9DS_0066.JPG';
 import textBoxVector from '../src/referenceImages/vectors/mainTitleVector.png';
 import highlight1 from '../src/referenceImages/9DS_0353.JPG';
 import backgroundImage2 from '../src/referenceImages/DSC_0530.JPG';
+import emailIcon from '../src/referenceImages/icons/emailIcon.png';
+import locationIcon from '../src/referenceImages/icons/locationIcon.png';
+import phoneIcon from '../src/referenceImages/icons/phoneIcon.png';
+import socialMediaIcon from '../src/referenceImages/icons/socialMediaIcon.png';
 
 class App extends React.Component {
     constructor(props) {
@@ -47,22 +51,22 @@ class App extends React.Component {
 render() {
   return (
       <div>
+      
         <section id="page1">
-        
-        <img className="titleImage d-block img-fluid mx-auto" src={textBoxVector}/>
+            <img className="titleImage d-block img-fluid mx-auto" src={textBoxVector}/>
 
-        <div className="d-flex justify-content-center">
-              <div className="p-2 titleOblique-text">So it would be a shame to not share a good story, right?</div>
-        </div>
+            <div className="d-flex justify-content-center">
+                  <div className="p-2 titleOblique-text">So it would be a shame to not share a good story, right?</div>
+            </div>
 
-        <div className="d-flex justify-content-center">
-              <div className="p-2 title-lineStyle"></div>
-        </div>
+            <div className="d-flex justify-content-center">
+                  <div className="p-2 title-lineStyle"></div>
+            </div>
 
-        <div className="d-flex scroll-info justify-content-center">
-            <div className="p-2 scroll-text">Scroll Down for Mainpage</div>
-        </div>
-      </section>
+            <div className="d-flex scroll-info justify-content-center">
+                <div className="p-2 scroll-text">Scroll Down for Mainpage</div>
+            </div>
+        </section>
       
 
       <div className="page-divider">
@@ -98,10 +102,7 @@ render() {
       </div>
     
 
-      <section id="page2">
-          <div className="imageContainer">
-            <img className="mainImage" src={backgroundImage}/>
-          </div>
+        <section id="page2">
             <article className="contentContainer">
                 <div className="d-flex justify-content-center headerStyle align-self-center">
                     <div className="p-2">Bryant Photography</div>
@@ -119,6 +120,10 @@ render() {
                       <button type="button" className="text-center btn btn-lg btn-light ">Portfolio</button>
                 </div>
             </article>
+    
+            <div className="imageContainer">
+                <img className="mainImage" src={backgroundImage}/>
+            </div>
         </section>
     
         
@@ -207,11 +212,76 @@ render() {
         
     
     <section id="page4">
+        <article className="contentContainer flexboxRestricted">
+            <div className="d-flex justify-content-left dividerHeader align-self-center">
+                <div className="p-2">Bio Information</div>
+            </div>
+            <br/>
+            <div className="d-flex justify-content-center infoStyle">
+                  <p className="p-2">My name is Blake Bryant, and, as you have probably already discovered, I have a love for photography. More specifically, I seek to capture “the asthetic”. This is satisfying sensation a viewer gets from, one, finding the image visually pleasing and, two, feeling a connection to the subject.</p>
+            </div>
+    
+            <div className="d-flex justify-content-center infoStyle">
+                  <p className="p-2">My favorite types of photography usually involve a drone and forested enviroment rich in detail, as you will see. I personally find Aerial photos to be the most effective way of capturing landscape photos due to the unique perspective and scope of the scenery that can be shown. However, I am comfortable with snapping photos in a variety of settings, ranging from highly urbanized cities to beaches, for types of photography consisting of, but not limited to, action shots, portraits, landscapes, and light painting.</p>
+            </div>
+        </article>
+        
         <div className="imageContainer2">
             <img className="mainImage2" src={backgroundImage2}/>
         </div>
-          
     </section>
+    
+    
+    <footer id="contactInfo">
+        <article className="footer-contentContainer">
+            <div className="d-flex justify-content-center align-self-center">
+                <div className="p-2">Contact Information</div>
+            </div>
+            <div className="d-flex justify-content-center align-self-center">
+                <div className="p-2">For business inquires, comments, or questions about my work:</div>
+            </div>
+            
+            <div className="footerLeftSection">
+                <div className="d-flex justify-content-left">
+                    <div className="p-2">
+                        <img className="icon" width="20" src={locationIcon}/>
+                    </div>
+                    <div className="p-2">Eugene, Oregon, USA</div>
+                </div>
+                <div className="d-flex justify-content-left">
+                    <div className="p-2">
+                        <img className="icon" width="20" src={emailIcon}/>
+                    </div>
+                    <div className="p-2">bryantb2@my.lanecc.edu</div>
+                </div>
+                <div className="d-flex justify-content-left">
+                    <div className="p-2">
+                        <img className="icon" width="20" src={phoneIcon}/>
+                    </div>
+                    <div className="p-2">541-505-0520</div>
+                </div>
+                <div className="d-flex justify-content-left">
+                    <div className="p-2">
+                        <img className="icon" width="20" src={socialMediaIcon}/>
+                    </div>
+                    <div className="p-2">bryantb2</div>
+                </div>
+            </div>
+    
+            <div className="d-flex justify-content-center">
+                <div className="p-2">&nbsp;</div>
+            </div>
+
+            <div className="footerRightSection">
+                <div className="d-flex justify-content-left">
+                    <div className="p-2">If you like what you see, please consider sharing this site:</div>
+                </div>
+                <div className="d-flex justify-content-left">
+                      <button type="button" className="text-center btn btn-lg btn-light ">Share</button>
+                </div>
+            </div>
+        </article>
+    </footer>
 
 
 
