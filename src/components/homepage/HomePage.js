a1c524957fdfc16b8f156260822c536125bd4d80
Blake <bryantb2@my.lanecc.edu>
2019-09-21 22:37:41 -0700

    optimization for title page (desktop and mobile versions) finished

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -109,18 +109,20 @@ class HomePage extends React.Component {
       return (
         <div id="homepage-wrapper">
             <section id="page1">
-                <img className="titleImage d-block img-fluid mx-auto" src={textBoxVector}/>
+                <div className="titleContentContainer">
+                    <img className="titleImage d-block img-fluid mx-auto" src={textBoxVector}/>
 
-                <div className="d-flex justify-content-center">
-                      <div className="p-2 titleOblique-text">So it would be a shame to not share a good story, right?</div>
-                </div>
+                    <div className="d-flex justify-content-center">
+                          <div className="p-2 titleOblique-text">So it would be a shame to not share a good story, right?</div>
+                    </div>
 
-                <div className="d-flex justify-content-center">
-                      <div className="p-2 title-lineStyle"></div>
-                </div>
+                    <div className="d-flex justify-content-center">
+                          <div className="p-2 title-lineStyle"></div>
+                    </div>
 
-                <div className="d-flex scroll-info justify-content-center scroll-text">
-                    <div className="p-2">Scroll Down for Mainpage</div>
+                    <div className="d-flex scroll-info justify-content-center scroll-text">
+                        <div className="p-2">Scroll Down for Mainpage</div>
+                    </div>
                 </div>
             </section>
       
