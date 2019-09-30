8a403fbecad742bf7d78bab9570536eb049ea1cc
Blake <bryantb2@my.lanecc.edu>
2019-09-29 21:17:43 -0700

    adding JSX and CSS to infinite scroll page

--- src/components/infiniteScrollPage/InfiniteScrollPage.js
+++ src/components/infiniteScrollPage/InfiniteScrollPage.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import './App.css';
 import './InfiniteScrollPage.css';
 import Navbar from '../navbar/Navbar.js';
 
-class InfiniteScrollPage extends React.Component() {
-    
-    
-    return (
-        <Navbar />
-    );
+class InfiniteScrollPage extends React.Component {
+    //INPUT: takes in a genre name and makes a fetch call to the server for the photo data
+    //page is loaded once the API call has completed
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <div id="InfiniteScrollPage">
+            <Navbar />
+            
+            <div className="d-flex justify-content-left">
+                <div className="p-2 infoStyle-GenrePage">
+                    {this.props.genre}
+                </div>
+                <div className="p-2">
+                    <div className="portfolioDropdown dropdown">
+                        <button className="genreSelectButton2 btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Genre
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" href="#">Urban</a>
+                            <a className="dropdown-item" href="#">Natural</a>
+                            <a className="dropdown-item" href="#">Aerial</a>
+                            <a className="dropdown-item" href="#">Portraits</a>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+         </div>
+        );
+    }
 }
 
 export default InfiniteScrollPage;
--- src/components/infiniteScrollPage/PortfolioPageSelection.js
+++ src/components/infiniteScrollPage/PortfolioPageSelection.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import './PortfolioPage.css';
+import Navbar from '../navbar/Navbar.js';
 
 function PortfolioPageSelection() {
     
     
     return (
         <div id="portfolioPage">
-            <nav className="navbar navbar-expand-lg navbar-dark customNavStyles-unFixed">
-
-              <a className="navbar-brand" href="#">Bryant Photography</a>
-
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Highlights<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Portfolio<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Share<span className="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-              </div>
-            </nav>
+            <Navbar />
     
             <article className="contentContainer customPortfolioContainer">
                 <div className="d-flex justify-content-center headerStyle-GenrePage">
