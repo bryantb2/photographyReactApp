ce5ec9538b08b4f4549eacb8529563d82a456859
Blake <bryantb2@my.lanecc.edu>
2019-10-30 01:20:16 -0700

    structural changes and cleaned up comments

--- server/app.js
+++ server/app.js
@@ -2,37 +2,28 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = requrie('cors');
 require('dotenv/config');
 
-//Middlewars
-/*
-functions that are executed along with certain routes
-*/
-
+//Middlewares: functions that are executed along with certain routes
+app.use(cors()); //alows for cross-domain communication with the web server
 app.use(bodyParser.json());
 
 //IMPORT ROUTES
-const imageRoute = require('./routes/image'); 
+const imageRoute = require('./routes/image');
 app.use('/image', imageRoute); //middleware example
 
 
-//ROUTES
-/*
-patch for updating
-delete for deleting
-get to retrieve info
-post to send info
-*/
+//ROUTES: patch for updating delete for deleting get to retrieve info post to send info
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('HOME');
 });
 
-
-
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log('connected to DB!')
-);
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true
+}, () => console.log('connected to DB!'));
 
 //tells express which port to listen on
 app.listen(3001);
