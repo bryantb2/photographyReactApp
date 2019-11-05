6dbf0b2106b61acf3dadbf834b583c114e715b2b
bryantb2 <bryantb2@my.lanecc.edu>
2019-11-05 14:11:16 -0800

    adding React router

--- photographyReactApp/src/App.js
+++ photographyReactApp/src/App.js
@@ -1,45 +1,41 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './History.js';
 import './App.css';
 import HomePage from '../src/components/homepage/HomePage.js';
-import PortfolioPageSelection from '../src/components/infiniteScrollPage/PortfolioPageSelection.js';
-
-//TESTING INFINITE SCROLL
-import PhotoPage from './components/infiniteScrollPage/InfiniteScrollPage.js';
-
-
+import PortfolioSelection from '../src/components/infiniteScrollPage/PortfolioPageSelection.js';
+import InfiniteScrollPage from './components/infiniteScrollPage/InfiniteScrollPage.js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             greeting: "hello there",
         }
-        
-        
+
+
         //BINDS
     }
-    
-    
+
+
     //METHODS
-    
-    
-    
-    //API CALL TEST
-    //1. call and store api
-    //2. pass api info into
-    
-        
-render() {
-    //props will be passed down into HomePage (will contain )
-      //<PortfolioPageSelection />
-      //
-    //<HomePage />
-    //
-  return (
-      <PhotoPage genre="Natural"/>
-    );
-  }
+
+    render() {
+        return (
+            <div className="ui container">
+            <Router history={History}>
+                <div>
+                    <Switch>
+                        <Route path="/" component={HomePage} />
+                        <Route path="/portfolioselection" exact component={PortfolioSelection} />
+                        <Route path="/gallery" exact component={InfiniteScrollPage} />
+                    </Switch>
+                </div>
+            </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
