d1d0e525730f5e20279d59ad8f6bd5af5a866895
bryantb2 <bryantb2@my.lanecc.edu>
2019-11-19 12:48:32 -0800

    rebuilding express server image.js file to use functions that simply the get/returning of images; restructured the routes based off their protocol type

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,8 +1,44 @@
-const express = require('express');
+c+onst express = require('express');
 const router = express.Router();
 const Image = require('../models/Image');
 const ImageContainer = require('../models/ImageContainer');
 const ContainerDirectory = require('../models/ContainerDirectory');
+require('dotenv/config');
+// using async and await key words removes the need to use extra syntax like .then and arrow functions
+
+// GET SPECIFIC IMAGE FROM A GENRE CATEGORY
+router.get('/:genre/:imageId', async (req, res) => {
+    try {
+        console.log(req.params);
+        const image = await Image.findById(req.params.imageId);
+        if (image == null) {
+            res.status(404);
+        }
+        else {
+            res.status(202);
+        }
+        res.json(image);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+});
+
+// GETS IMAGES BASED OFF GENRE
+router.get('/genre/:genre', async (req, res) => {
+    try {
+        // TODO: ASK MARI IF AWAIT IS REQUIRED WHEN CALLING THE FUNCTION
+        const imageArray = await GetAndReturnGenreArray(req.params.gerne);
+        console.log(imageArray);
+        res.json(imageArray);
+        
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+});
 
 // GETS ALL IMAGES
 router.get('/', async (req, res) => {
@@ -15,49 +51,6 @@ router.get('/', async (req, res) => {
         });
     }
 });
-
-// GETS IMAGES BASED OFF GENRE
-router.get('/genre/:genre/', async (req, res) => {
-    try {
-        // search the DB for the document with storingImages prop set to false
-            // get the objectID from the appropriate genre object prop
-            // use the genre object ID to search for the appropriate storage document
-            // return image array from imageArray property
-        const container = await ContainerDirectory.findById({
-            _id: "5dcdc3ca1c9d440000f857b1"
-        });
-        
-        const requestedGenre = req.params.genre;
-        let genreID;
-        if(requestedGenre === "urban") {
-            genreID = container.urban;
-        }
-        else if(requestedGenre === "natural") {
-            genreID = container.natural;
-        }
-        else if(requestedGenre === "aerial") {
-            genreID = container.aerial;
-        }
-        else {
-            genreID = container.portraits;
-        }
-        
-        const genreObject = await ImageContainer.findById({
-            _id: genreID
-        });
-        const images = genreObject.imageArray;
-        
-        console.log(container);
-        res.json(container);
-        
-    } catch (err) {
-        res.json({
-            message: err
-        });
-    }
-});
-
-// using async and await key words removes the need to use extra syntax like .then and arrow functions
 
 // SUBMITS AN IMAGE
 router.post('/', async (req, res) => {
@@ -78,47 +71,6 @@ router.post('/', async (req, res) => {
             message: err
         });
     }
-});
-
-// GET SPECIFIC IMAGE
-router.get('/:imageId', async (req, res) => {
-    try {
-        console.log(req.params);
-        const image = await Image.findById(req.params.imageId);
-        if (image == null) {
-            res.status(404);
-        }
-        else {
-            res.status(202);
-        }
-        res.json(image);
-    } catch (err) {
-        res.json({
-            message: err
-        });
-    }
-});
-
-
-// DELETE A SPECIFIC IMAGE
-router.delete('/:imageId', async (req, res) => {
-    try {
-        const removedImage = await Image.remove({
-            _id: req.params.imageId
-        });
-        if (removedImage == null) {
-            res.status(404).send("An image with that ID was not found");
-        }
-        else {
-            res.status(202).send("Success, that SOB is GONE!");
-        }
-        res.json(removedImage);
-    } catch (err) {
-        res.json({
-            message: err
-        });
-    }
-
 });
 
 
@@ -148,5 +100,65 @@ router.patch('/:imageId/:property', async (req, res) => {
     }
 });
 
+// DELETE A SPECIFIC IMAGE
+router.delete('genre/:genre/:imageId', async (req, res) => {
+    try {
+        const removedImage = await Image.remove({
+            _id: req.params.imageId
+        });
+        if (removedImage == null) {
+            res.status(404).send("An image with that ID was not found");
+        }
+        else {
+            res.status(202).send("Success, that SOB is GONE!");
+        }
+        res.json(removedImage);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+
+});
+
+
+// SERVER FUNCTIONS
+function GetAndReturnGenreArray(genreNameAsString) {
+    // searchs the specified DB collection for the document with the objectId set in the .env file
+            // finds and gets the document by objectId
+            // accesses the genre sub-object, which is a series of key/value pairs for genre names and their document objectIds
+            // Gets the appropriate genre document ID 
+            // Uses the genre document ID to find the document storing an array of image objects
+            // return the array of image objects
+        const requestedGenre = genreNameAsString;
+        const directoryId = process.env.DIRECTORY_ID;
+        const directoryContainer = await ContainerDirectory.findById({
+            _id: directoryId
+        });
+       
+        const genreDirectory = directoryContainer.genre;
+        let genreID;
+        if(requestedGenre === "urban") {
+            genreID = genreDirectory.urban;
+        }
+        else if(requestedGenre === "natural") {
+            genreID = genreDirectory.natural;
+        }
+        else if(requestedGenre === "aerial") {
+            genreID = genreDirectory.aerial;
+        }
+        else {
+            genreID = genreDirectory.portraits;
+        }
+        
+        // NOTE: when attempt to find an image container by id, the genreID object MUST be toString-ed
+        
+        const genreObject = await ImageContainer.findById({
+            _id: genreID.toString()
+        });
+        const images = genreObject.imageArray;
+    
+        return images;
+}
 
 module.exports = router;
