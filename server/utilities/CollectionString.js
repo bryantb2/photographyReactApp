59b0bfab1ec3f36cdc0e9a18d2fa45cf14b02000
bryantb2 <bryantb2@my.lanecc.edu>
2019-11-21 16:22:46 -0800

    added static class for collection names

---
+++ server/utilities/CollectionString.js
@@ -0,0 +1,40 @@
+// This will return a class with a static getter for the collection name
+// Done this way because it hides the internal collection object that stores the names of the collections
+// Requires the server admin to manually go into this file and add, remove, or edit a key-value pair in the collections object
+let ImageCollectionNames(()=> {
+    // names of collections
+    let collections = {
+        collection1: "Aerial",
+        collection2: "Natural",
+        collection3: "Portraits",
+        collection4: "Urban"
+    };
+    
+    
+    function AddCollectionVarsToArray() {
+        // this function will package all the collection variables into the colleciton array
+        let tempCollectionArray = new Array();
+        const collectionVarName = "collection";
+        let noMoreCollectionVariables = false;
+        let counter = 1;
+        while(noMoreCollectionVariables === false) {
+            // dynamically get the collection variable
+            // test if it is null and increment counter if the variable exists
+            let tempVariable = collections[collectionVarName + counter];
+            if(tempVariable !== null || tempVariable !== undefined) {
+                tempCollectionArray.push(tempVariable)
+                counter++
+            }
+            else {
+                noMoreCollectionVariables = true;
+            }
+        }
+        return tempCollectionArray;
+    }
+    
+    return class {
+        static get CollectionNameArray() {
+            return AddCollectionVarsToArray();
+        }
+    }
+});
